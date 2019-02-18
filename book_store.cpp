99304852569ec27d6acfb12994fee21e486e48bd
saguywalker <guyguy252@gmail.com>
2019-02-19 02:38:36 +0700

    Create book store program.

---
+++ book_store.cpp
@@ -0,0 +1,21 @@
+/*
+    College book store
+*/
+
+#include <iostream>
+
+using namespace::std;
+
+class Book:
+    private:
+        int code;           //the book code
+        float price;        //single copy price
+        int inventory;         //number on hand
+        int prosEnrollment; //prospective enrollment
+        bool isRequest;     //true => request, false => optional
+        bool isNew;         //true => new, false => used
+
+int main(){
+
+    return 0;
+}
