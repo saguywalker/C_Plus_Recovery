85cc80524e3265dd33fb1e061612cfb7a77ef584
saguywalker <guyguy252@gmail.com>
2019-02-19 02:49:19 +0700

    Add book's set function.

--- book_store.cpp
+++ book_store.cpp
@@ -6,7 +6,7 @@
 
 using namespace::std;
 
-class Book:
+class Book{
     private:
         int code;           //the book code
         float price;        //single copy price
@@ -14,8 +14,36 @@ class Book:
         int prosEnrollment; //prospective enrollment
         bool isRequest;     //true => request, false => optional
         bool isNew;         //true => new, false => used
+    public:
+        void set(int _code, float _price, int _inventory, int _enroll, int _request, int _new){
+            code = _code;
+            price = _price;
+            inventory = _inventory;
+            prosEnrollment = _enroll;
+            isRequest = _request;
+            isNew = _new;
+        }
+};
 
 int main(){
+    int code, inventory, prosEnrollment;
+    float price;
+    bool isRequest, isNew;
+    cout << "Please enter the book code: ";
+    cin >> code;
+    cout << "single copy price: ";
+    cin >> price;
+    cout << "number on hand: ";
+    cin >> inventory;
+    cout << "prospective enrollment: ";
+    cin >> prosEnrollment;
+    cout << "1 for reqd/0 for optional: ";
+    cin >> isRequest;
+    cout << "1 for new/0 for used: ";
+    cin >> isNew;
+
+    Book b1;
+    b1.set(code, price, inventory, prosEnrollment, isRequest, isNew); 
 
     return 0;
 }
