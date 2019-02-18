1724fc85d8d1c017b3938d1ce1a63f610448484e
saguywalker <guyguy252@gmail.com>
2019-02-19 01:30:04 +0700

    Create megic number finder program.

---
+++ magic_num.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <sstream>
+
+using namespace::std;
+
+bool isPerfectSquare(int &n){
+    int i = 2;
+    while(i <= n / 2){
+        if(i*i == n){
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+bool isSumSeries(int &n){
+    int sum = 0;
+    int i = 1;
+    while(sum < n){
+        sum += i;
+        if (sum == n){
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+int main(int argc, char **argv){
+    /*check number from cmd argument.
+    std::istringstream ss(argv[1]);
+    int x;
+    if (!(ss >> x)){
+        std::cerr << "Invalid number: " << argv[1] << endl;
+    }*/
+
+    int num = 1226;
+    while(true){
+        if (isPerfectSquare(num) && isSumSeries(num)){
+            cout << "Magic number was found! => " << num << endl;
+            break;
+        }
+        num++;
+    }
+
+    return 0;
+}
